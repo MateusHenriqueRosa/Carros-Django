ab3f163166370c9656faa3ed1281d1c2f01ae7fb
Mateus Henrique Rosa <mateus.rosa@unifebe.edu.br>
2025-06-16 21:26:23 -0300

    feat: adicionado filtro na query do banco

--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,6 @@ from cars.models import Car
 
 def cars_view(request):
 
-    cars = Car.objects.all()
+    cars = Car.objects.filter(model__contains="Opala")
 
     return render(request, "cars.html", {"cars": cars})
