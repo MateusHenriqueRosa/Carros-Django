ba0f11d68aea52b7366615dd1e7524e572ff2502
Mateus Henrique Rosa <mateus.rosa@unifebe.edu.br>
2025-09-09 19:40:06 -0300

    feat: bio gerada por ia com api da opanai

--- cars/signals.py
+++ cars/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
@@ -19,7 +20,10 @@ def car_post_save(sender, instance, **kwargs):
 def car_post_delete(sender, instance, **kwargs):
     car_inventory_update()
 
+
 @receiver(pre_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
     if not instance.bio:
+        # Comentado a linha abaixo para não gastar tokens desnecessários
+        # ia_bio = get_car_ai_bio(instance.model, instance.brand, instance.model_year)
         instance.bio = "Bio gerada automaticamente!"
