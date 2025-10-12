ffecad80f8a7cd362ea19b9041a8134fb579dac6
Mateus Henrique Rosa <mateus.rosa@unifebe.edu.br>
2025-10-12 14:46:57 -0300

    feat: dockerizacao do projeto

--- app/settings.py
+++ app/settings.py
@@ -78,14 +78,20 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carros',
+        'NAME': 'carrosdjango',
         'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'HOST': 'carros_db',
         'PORT': '5432',
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
